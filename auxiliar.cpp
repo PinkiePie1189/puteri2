757b6e758dc6d4cee1386bd81216902341c51009
PinkiePie1189 <mircea_costin84@yahoo.com>
2016-02-25 13:11:02 +0200

    Update auxiliar.cpp

--- auxiliar.cpp
+++ auxiliar.cpp
@@ -7,14 +7,14 @@ int main()
   const int p=100019;
   for(int a=1;a<=500;a++)
     {
-      int rata=1;
-      int put=1;
-      while(put!=a)
+      int rata=0;
+      int put=a;
+      do
       {
        put*=a; //a<p ,deci a%p==a
        put%=p;
        rata++;
-      }
+      }while(put!=a);
      fprintf(fout,"%d\n",rata);
     }
 }
