5a68a7ce6f26a5f307a5ed2c81cd56c61714ab66
MoonfireSeco <haaloplaysminecraft@gmail.com>
2016-02-25 16:04:53 +0200

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,67 @@
-#include<cstdio>
-#define L 10000
-#define rata 100018
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
 using namespace std;
-FILE*fin=fopen("puteri2.in","r");
-FILE*fout=fopen("puteri2.out","w");
-long long a[L],b[L];
+long long a[10000];
+long long b[10000];
+
+unsigned long long put(long long a,long long b)
+{
+    unsigned long long res=1;
+    for(register int i=1;i<=b;i++)
+    {
+        res*=a;
+    }
+    return res;
+}
 int main()
 {
-   //const int p=nr ala din pb;
-   int N;
-   long long s=0;
-   fscanf(fin,"%d",&N);
-   for(int i=0;i<=N;i++)
-    fscanf(fin,%lld,&a[i];
-   for(int i=0;i<=N;i++)
-    {fscanf(fin,%lld,&b[i]);
-     b[i]%=rata;
-     if(b[i]==0) b[i]=rata;
+    //freopen("in.txt","r",stdin);
+    freopen("puteri2.in","r",stdin);
+    freopen("puteri2.out","w",stdout);
+    int load;
+    cin>>load;
+    for(register int i=0;i<=load-1;i++)
+    {
+        scanf("%d",&a[i]);
     }
-   for(int i=1;i<=N;i++)
-   {
-    int put=1;
-    for(int i=1;i<=b[i];i++)
-     {
-       put*=a
-       put%=p;
-     }
-     s+=put;
-     s%=p;
-   }
- fprintf(fout,%lld,s%p);
+    for(register int i=0;i<=load-1;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    unsigned long long s;
+    unsigned long long p=0;
+    int exp;
+    unsigned long long puto;
+    for(register int i=0;i<=load-1;i++)
+    {
+        s=1;
+        if(a[i]==1 || b[i]==0)
+        {
+            goto end;
+        }
+        puto=1;
+        exp=0;
+        while(puto<=100019 && exp<b[i])
+        {
+            puto*=a[i];
+            //cout<<puto<<endl;
+            exp++;
+        }
+        puto%=100019;
+        //cout<<"pentru "<<a[i]<<" avem puterea "<<puto<<" cu exponentul "<<exp<<endl;
+        for(register int k=1;k<=b[i]/exp;k++)
+        {
+            s*=puto;
+            s%=100019;
+        }
+        for(register int k=1;k<=b[i]%exp;k++)
+        {
+            s*=a[i];
+            s%=100019;
+        }
+        end:
+        p+=s;
+    }
+    cout<<p%100019;
 }
